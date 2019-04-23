41ac644c06f2111e72507f7b48c5ad080452babf
Suraj Bhooshan <surajbhooshan@gmail.com>
2019-04-23 17:02:21 +0530

    Commited braces verifier

---
+++ amazon_truck.py
@@ -0,0 +1,12 @@
+
+
+def get_optimized_delivery_plan(N, all_locations, no_of_deliveries):
+
+    # Initialized final output
+    optimized_path = []
+
+
+
+
+# Pass dynamic inputs
+get_optimized_delivery_plan(3, [[1,2],[3,4],[1,-1]], 2)
